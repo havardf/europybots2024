5025ae27d56a960e76b9072ddb07bd2dbb77a93d
Håvard Futsæter <havardf@met.no>
2024-10-30 09:36:05 +0100

    Somewthing

--- bot.py
+++ bot.py
@@ -24,21 +24,38 @@ class Bot:
     def __init__(self):
         self.team = "TeamName"  # This is your team name
         # This is the course that the ship has to follow
+        # self.course = [
+        #     Checkpoint(latitude=43.797109, longitude=-11.264905, radius=50),
+        #     Checkpoint(longitude=-29.908577, latitude=17.999811, radius=50),
+        #     Checkpoint(latitude=-11.441808, longitude=-29.660252, radius=50),
+        #     Checkpoint(longitude=-63.240264, latitude=-61.025125, radius=50),
+        #     Checkpoint(latitude=2.806318, longitude=-168.943864, radius=1990.0),
+        #     Checkpoint(latitude=-62.052286, longitude=169.214572, radius=50.0),
+        #     Checkpoint(latitude=-15.668984, longitude=77.674694, radius=1190.0),
+        #     Checkpoint(latitude=-39.438937, longitude=19.836265, radius=50.0),
+        #     Checkpoint(latitude=14.881699, longitude=-21.024326, radius=50.0),
+        #     Checkpoint(latitude=44.076538, longitude=-18.292936, radius=50.0),
+        #     Checkpoint(
+        #         latitude=config.start.latitude,
+        #         longitude=config.start.longitude,
+        #         radius=5,
+        #     ),
+        # ]
         self.course = [
-            Checkpoint(latitude=43.797109, longitude=-11.264905, radius=50),
-            Checkpoint(longitude=-29.908577, latitude=17.999811, radius=50),
-            Checkpoint(latitude=-11.441808, longitude=-29.660252, radius=50),
-            Checkpoint(longitude=-63.240264, latitude=-61.025125, radius=50),
-            Checkpoint(latitude=2.806318, longitude=-168.943864, radius=1990.0),
-            Checkpoint(latitude=-62.052286, longitude=169.214572, radius=50.0),
-            Checkpoint(latitude=-15.668984, longitude=77.674694, radius=1190.0),
-            Checkpoint(latitude=-39.438937, longitude=19.836265, radius=50.0),
-            Checkpoint(latitude=14.881699, longitude=-21.024326, radius=50.0),
-            Checkpoint(latitude=44.076538, longitude=-18.292936, radius=50.0),
-            Checkpoint(
-                latitude=config.start.latitude,
-                longitude=config.start.longitude,
-                radius=5,
+                Checkpoint(latitude=27.9774487576345, longitude=-65.06541454115296, radius=50),
+                Checkpoint(latitude=14.77065458187063, longitude=-68.82624251716636, radius=50),
+                Checkpoint(latitude=10.157187507847667, longitude=-80.09127381434787, radius=50),
+                Checkpoint(latitude=6.682150603618609, longitude=-78.71306517737868,radius=50), # Panama canal finished
+                Checkpoint(latitude=5.480840563786344, longitude=-79.23386963026627,radius=50),
+                Checkpoint(latitude=4.483046736485534, longitude=-85.89829886898251,radius=50),
+                Checkpoint(latitude=4.329411363655353, longitude=-92.71681895714882,radius=50),
+                Checkpoint(latitude=21.169951623924405, longitude=-125.4226017529291,radius=50),
+                Checkpoint(latitude=2.806318, longitude=-168.943864, radius=1990.0),
+                Checkpoint(latitude=-15.668984, longitude=77.674694, radius=1190.0),
+                Checkpoint(
+                    latitude=config.start.latitude,
+                    longitude=config.start.longitude,
+                    radius=5,
             ),
         ]
 
